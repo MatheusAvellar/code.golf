16335dd640ea5e8d5e7aaa17a2657a19b0d6b6d3
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2021-05-22 00:36:04 -0300

    Add ><>, Powershell alternate

---
+++ fibonacci/fish.fish
@@ -0,0 +1,7 @@
+# For code.golf/fibonacci#fish
+##############################
+# [Ref] Comment by @randomra:
+# https://codegolf.stackexchange.com/a/1909/66000
+# 20 bytes/chars (16 solution exists):
+01r:nao$:@+$rl">"=?;
+##############################
