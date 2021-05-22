9bf4320a1b49b7f5ec2dc80346c839bcce2a52ee
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2021-05-22 01:02:09 -0300

    Add Swift

---
+++ fibonacci/swift.swift
@@ -0,0 +1,5 @@
+// For code.golf/fibonacci#swift
+////////////////////////////////
+// 48 bytes/chars (46 solution exists):
+var a=1,b=1;for _ in 0..<31{a=b-a;print(a);b+=a}
+////////////////////////////////
